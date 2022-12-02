c633def60b401c62ad1acf29c53595bd2b28bb1b
Vektor1horn <moritz.kallenbach@t-online.de>
2022-12-02 13:56:52 +0100

    PICO_BLINK

--- src/main.c
+++ src/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#define LED_PIN 0
 
 int main(void)
 {  
     stdio_init_all();
     printf("\n\nHello World\n");
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true)
     {
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        gpio_put(LED_PIN, 1);
         sleep_ms(100);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        gpio_put(LED_PIN, 0);
         sleep_ms(100);
     }
 }
