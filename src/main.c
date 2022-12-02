3621c5acb1fba276010246f093efd67bd8f62d49
Vektor1horn <moritz.kallenbach@t-online.de>
2022-12-02 15:29:32 +0100

    Co-authored-by: freluzi <freluzi@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,18 +1,77 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#define LED_PIN 0
+#include "hardware/timer.h"
+#include "hardware/gpio.h"
+
+#define PIN_D0 18           //Ausgangspin für PWM
+#define PIN_BUTTON 15       //Sensorpin für den Button
+#define INTERRUPT_TIME 15   //Anzahl der us nachdem der Timer erneut aufgerufen werden soll
+
+//volatile uint16_t TimerReset = 0;
+volatile uint16_t TimerCount = 0;
+volatile uint16_t PotiRead = 0;
+volatile bool ButtonPressed = false;
+
+bool reapting_timer_callback(struct reapeating_timer *t)
+{
+    //wenn der Timer 0 ist soll PWM 1 sein
+    if(TimerCount == 0) 
+    {
+        gipo_put(PIN_D0, 1);
+    }
+
+    //wenn der Timer gleich 1 ms + die PotiReadZeit erreicht hat soll PWM 0 sein
+    if(TimerCount == PotiRead*(1000/INTERRUPT_TIME)) 
+    {
+        gpio_put(PIN_D0, 0);
+    }
+
+    //erhöht den Timer Count um 1
+    TimerCount++; //erhöht den Timer Count um 1
+
+    //Wenn 10 ms erreicht sind soll PWM wieder von vorne anfangen --> TimerCount 0
+    if(TimerCount >= 10000/INTERRUPT_TIME) 
+    {
+        TimerCount = 0;
+    }
+
+    //wenn Knopf gedrückt und in der Mitte des PWM Intervalls 
+    if(gpio_get(PIN_BUTTON) == 1 && TimerCount == (int) 5000/INTERRUPT_TIME && ButtonPressed == false)
+    {
+        gpio_put(PIN_D0,1);
+        ButtonPressed = true;
+    
+    }
+
+    if(TimerCount == 2 + (int)5000/INTERRUPT_TIME)
+    {
+        gpio_put(PIN_D0, 0);
+    }
+   
+
+    return true;
+}
 
 int main(void)
 {  
     stdio_init_all();
-    printf("\n\nHello World\n");
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true)
+    gpio_init(PIN_D0);
+    gpio_init(PIN_BUTTON);
+    gpio_set_dir(PIN_D0, GPIO_OUT);
+    gpio_set_dir(PIN_BUTTON, GPIO_IN);
+
+    struct repeating_timer timer;
+    add_reapating_timer_us(INTERRUPT_TIME, reapting_timer_callback, NULL, &timer);
+    
+    while(1)
     {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(100);
+        
+        // alle Funktionen die auserhalb des Timer gemacht werden sollen
+        PotiRead = adc_read();                                      //Potentiometer wird ausgelesen 
+        PotiRead = map(PotiRead, 0, 4096, 1000, 2000);              //Potentiometerdaten werden auf zwischen 1000 und 2000 gelegt
+        if(ButtonPressed == true && gpio_get(PIN_BUTTON) == 0)      
+        {
+            ButtonPressed = false;
+        }
     }
 }
