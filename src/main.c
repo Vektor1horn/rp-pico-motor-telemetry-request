f81acfb1042cf98072ff6e1004a2be32c2d4ddd4
Vektor1horn <moritz.kallenbach@t-online.de>
2023-01-20 10:01:17 +0100

    Neus Jahr Synchro

--- src/main.c
+++ src/main.c
@@ -10,17 +10,21 @@
 
 #define PIN_BUTTON 15       //Sensorpin für den Button
 #define PWM_PIN 18          //PWM Pin
+#define PEAK_PIN 19
+#define READ_PIN 20
+#define INTERRUPT_TIME 10 
+#define ALARM_TIMER 3
 
 
 
-//volatile uint16_t TimerReset = 0;
-volatile uint16_t TimerCount = 0;
+
 volatile uint16_t PotiRead = 0;
-volatile bool ButtonPressed = false;
+//volatile bool ButtonPressed = false;
 
-/* int set_pwm_freq (uint slice, int freq, uint32_t *div, uint32_t *top);
-int set_pwm_duty (uint slice, uint channel, uint32_t top, uint32_t duty); */
 void set_pwm_pin(uint pin, uint freq, uint duty_c);
+void repeating_timer_callback(struct repeating_timer *t);
+uint64_t alarm_callback(alarm_id_t id, void *user_data);
+void sendPeak();
 
 //Map nimmt einen Wert in einer Range und findet den dazu passenden Wert in einer neuen Range
 long map(long x, long in_min, long in_max, long out_min, long out_max);
@@ -32,6 +36,8 @@ int main(void)
     stdio_init_all(); 
     gpio_init(PIN_BUTTON);
     gpio_set_dir(PIN_BUTTON, GPIO_IN);
+    gpio_init(PEAK_PIN);
+    gpio_set_dir(PEAK_PIN, GPIO_IN);
 
     adc_init();
     adc_gpio_init(26);
@@ -39,8 +45,9 @@ int main(void)
     set_pwm_pin(PWM_PIN,100,1000);
 
     //inizialisierung des Timers
-    /* struct repeating_timer timer;
-    add_repeating_timer_us(INTERRUPT_TIME, repeating_timer_callback, NULL, &timer); */
+    struct repeating_timer timer;
+    add_repeating_timer_ms(INTERRUPT_TIME, repeating_timer_callback, NULL, &timer); 
+    
     
     while(1)
     {
@@ -48,10 +55,7 @@ int main(void)
         // alle Funktionen die auserhalb des Timer gemacht werden sollen
         PotiRead = adc_read();                                      //Potentiometer wird ausgelesen 
         PotiRead = map(PotiRead, 0, 4096, 1000, 2000);              //Potentiometerdaten werden auf zwischen 1000 und 2000 gelegt
-        if(ButtonPressed == true && gpio_get(PIN_BUTTON) == 0)      
-        {
-            ButtonPressed = false;
-        }
+        
         pwm_set_gpio_level(PWM_PIN, PotiRead);
 
     }
@@ -74,44 +78,34 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
     return (x - in_min) * (out_max - out_min)/ (in_max - in_min) + out_min;
 }
 
-/* bool repeating_timer_callback(struct repeating_timer *t)
+void repeating_timer_callback(struct repeating_timer *t)
 {
-    //wenn der Timer 0 ist soll PWM 1 sein
-    if(TimerCount == 0) 
-    {
-        gpio_put(PIN_D0, 1);
-    }
 
-    //wenn der Timer gleich 1 ms + die PotiReadZeit erreicht hat soll PWM 0 sein
-    if(TimerCount == PotiRead*(1000/INTERRUPT_TIME)) 
-    {
-        gpio_put(PIN_D0, 0);
-    }
-
-    //erhöht den Timer Count um 1
-    TimerCount++; //erhöht den Timer Count um 1
-
-    //Wenn 10 ms erreicht sind soll PWM wieder von vorne anfangen --> TimerCount 0
-    if(TimerCount >= 10000/INTERRUPT_TIME) 
-    {
-        TimerCount = 0;
-    }
-
-    //wenn Knopf gedrückt und in der Mitte des PWM Intervalls 
-    if(gpio_get(PIN_BUTTON) == 1 && TimerCount == (int) 5000/INTERRUPT_TIME && ButtonPressed == false)
-    {
-        gpio_put(PIN_D0,1);
-        ButtonPressed = true;
+    if(gpio_get(READ_PIN) != 0){
     
+       //add_alarm_in_ms(ALARM_TIMER, alarm_callback, NULL,false);
+       printf("mist");
     }
+    else{
 
-    if(TimerCount == 2 + (int)5000/INTERRUPT_TIME)
-    {
-        gpio_put(PIN_D0, 0);
+        sendPeak();
     }
-   
+  
+}
 
-    return true;
-} */
+uint64_t alarm_callback(alarm_id_t id, void *user_data){
+    sendPeak();
+    return 0;
+}
 
+void sendPeak(){
+    
+    
+    gpio_put(PEAK_PIN, 1);
+    sleep_us(100);
+    gpio_put(PEAK_PIN, 0);
+    printf("jawoll");
+    
+
+}
 
